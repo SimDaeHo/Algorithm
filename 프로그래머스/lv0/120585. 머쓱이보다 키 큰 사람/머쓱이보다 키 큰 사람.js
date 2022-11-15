7a54239afb1b849d45b1d07f17e729f451a7bae9
SimDaeHo <116733236+SimDaeHo@users.noreply.github.com>
2022-11-15 11:39:43 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -2,3 +2,24 @@ function solution(array, height) {
   const result =  array.filter(tall => tall > height).length
     return result
 }
+
+function solution(array, height) {
+  let count = 0
+  for(let i = 0; i < array.length; i++) {
+      if(array[i] >height) {
+          count ++
+      }
+  }
+     return count
+}
+
+function solution(array, height) {
+  let count = []
+  for(let i = 0; i < array.length; i++) {
+      if(array[i] >height) {
+          count.push(i)
+      }
+  }
+     return count.length
+}
+
