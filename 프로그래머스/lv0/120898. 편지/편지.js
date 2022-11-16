735bbd3f10b624752c059c0b4cbb7d12b9b3456a
SimDaeHo <116733236+SimDaeHo@users.noreply.github.com>
2022-11-16 20:23:56 +0900

    [level 0] Title: 편지, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120898. 편지/편지.js
@@ -0,0 +1,35 @@
+function solution(message) {
+    return message.length * 2
+}
+
+function solution(message) {
+    for(let i =0; i < message.length; i++) {
+        return message.length * 2
+    }
+}
+
+function solution(message) {
+    for(let num of message) {
+        return message.length * 2
+    }
+}
+
+
+
+function solution(message) {
+    let answer = 0
+    for(let i =0; i <message.length; i++) {
+        answer += 2
+        
+    }
+         return answer
+}
+
+function solution(message) {
+    let answer = 0
+    for(let x of message) {
+        answer += 2
+    }
+        return answer
+}
+
