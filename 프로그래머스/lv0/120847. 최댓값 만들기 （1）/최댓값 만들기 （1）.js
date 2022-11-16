2f373e7af114b629d016599a58d69d4706b5c55a
SimDaeHo <116733236+SimDaeHo@users.noreply.github.com>
2022-11-16 17:57:46 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -0,0 +1,18 @@
+function solution(numbers) {
+   
+}
+
+
+function solution(numbers) {
+    numbers.sort((a,b)=>b-a);
+    return numbers[0]*numbers[1];
+}
+
+function solution(numbers) {
+   let mx1 =0 
+   let mx2 = 0
+   mx1 = Math.max(...numbers) //numbers = [1,2,3,4,5] mx1 =5
+   numbers.splice(numbers.indexOf(mx1),1) //mx1의 인덱스부터 1개를 지운다 number =[1,2,3,4]
+   mx2 = Math.max(...numbers) // mx2=4
+   return mx1 * mx2 // 5*4
+}
