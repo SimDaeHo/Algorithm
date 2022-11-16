f3109899a20233fea82d633308b24b3b09033bdf
SimDaeHo <116733236+SimDaeHo@users.noreply.github.com>
2022-11-17 01:07:19 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
+++ 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
@@ -1,3 +1,27 @@
 function solution(slice, n) {
     return Math.ceil(n / slice)
 }
+
+
+function solution(slice, n) {
+    let i = 1;
+    while(slice*i<n){
+        i++
+    }
+    return i;
+}
+
+
+function solution(slice, n) {
+    var answer = 0;
+    for (let i = 0; i < n; i++) {
+        if (i < n) {
+            if (i % slice === 0) {
+                answer ++
+            }
+        }
+    }
+    return answer;
+}
+
+
