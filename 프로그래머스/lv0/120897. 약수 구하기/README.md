2e1785eb14ce02ed6eda00cd67e1a006c766073a
SimDaeHo <116733236+SimDaeHo@users.noreply.github.com>
2022-11-26 00:03:14 +0900

    [level 0] Title: 약수 구하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120897. 약수 구하기/README.md
+++ 프로그래머스/lv0/120897. 약수 구하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 약수 구하기 - 120897 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120897?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120897) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.13 ms
+메모리: 33.4 MB, 시간: 0.06 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.js
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.js
@@ -1,9 +1,16 @@
+// function solution(n) {
+//     arr = []
+//     for(i=1; i<=n; i++){
+//         if(n%i === 0){
+//             arr.push(i)
+//         }
+//     }
+//     return arr;
+// }
+
+
 function solution(n) {
-    arr = []
-    for(i=1; i<=n; i++){
-        if(n%i === 0){
-            arr.push(i)
-        }
-    }
-    return arr;
+  return Array.from({ length: n }, (_, i) => i + 1).filter(
+    (el) => n % el === 0
+  );
 }
