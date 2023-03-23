2bab3cc96d54d2f7ef21a96956817d0357499d37
SimDaeHo <116733236+SimDaeHo@users.noreply.github.com>
2023-03-23 17:54:23 +0900

    [level 0] Title: 배열의 유사도, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
@@ -5,3 +5,15 @@ s2.indexOf(v) != -1 ? answer++ : 1
 })
 return answer;
 }
+
+
+
+function solution(s1, s2) {
+    var answer = 0;
+    for (var i = 0; i < s1.length; i++){
+        for (var j = 0; j < s2.length; j++){
+            if(s1[i] === s2[j]) answer++;
+        }
+    }
+    return answer;
+}
