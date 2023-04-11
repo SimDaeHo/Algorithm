577da6959ffe29e641fc16e783dbae0527ac4349
SimDaeHo <116733236+SimDaeHo@users.noreply.github.com>
2023-04-11 12:06:47 +0900

    [level 0] Title: 순서쌍의 개수, Time: 7.63 ms, Memory: 36.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -0,0 +1,15 @@
+function solution(n) {
+    var answer = 0;
+    let cnt = 1;
+    let maxNumber = 1000001
+    while(true) {
+        if(cnt > n) {
+            break;
+        }
+        if(n % cnt === 0) {
+            answer = answer + 1
+        }
+        cnt = cnt + 1
+    }
+    return answer;
+}
