6629eb6f277af3a23b424fa47e168572d013b2d7
SimDaeHo <116733236+SimDaeHo@users.noreply.github.com>
2023-06-20 21:12:26 +0900

    [unrated] Title: 원하는 문자열 찾기, Time: 0.12 ms, Memory: 33.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
@@ -0,0 +1,10 @@
+function solution(myString, pat) {
+  const lowerCaseMyString = myString.toLowerCase();
+  const lowerCasePat = pat.toLowerCase();
+
+  if (lowerCaseMyString.includes(lowerCasePat)) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
