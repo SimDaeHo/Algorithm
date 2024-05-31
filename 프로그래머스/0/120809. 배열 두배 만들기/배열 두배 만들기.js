ae5bc636729058ed6e61a8cd6fcff3ae901358ea
SimDaeHo <116733236+SimDaeHo@users.noreply.github.com>
2024-05-31 11:27:45 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.09 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.js
@@ -0,0 +1,16 @@
+function solution(numbers) {
+    let answer = [];
+    for(let i=0; i < numbers.length; i++) {
+        answer.push(numbers[i] * 2)
+    }
+    return answer
+}
+
+
+function solution(numbers) {
+    let arr = numbers.map(number => number *2)
+    return arr
+    
+   
+   
+}
