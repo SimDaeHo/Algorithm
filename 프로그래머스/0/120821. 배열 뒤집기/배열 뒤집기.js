ec961b4db684cd273b7372d9f3b5c378c26024aa
SimDaeHo <116733236+SimDaeHo@users.noreply.github.com>
2024-06-19 09:51:12 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -0,0 +1,16 @@
+// function solution(num_list) {
+//    return num_list.reverse()
+// }
+
+
+function solution(num_list) {
+
+    let arr = [];
+    for(let i = num_list.length-1 ;i >= 0;i--)
+        {
+
+            arr.push(num_list[i]);
+
+        }
+    return arr
+}
